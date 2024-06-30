90f3b6d3ab6d42d4f27c0fd98ab2e86f42a658e1
Polaout <78549401+Polaout@users.noreply.github.com>
2024-06-30 07:16:55 +0000

    Initial Files

---
+++ WebAplication/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a rota principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
